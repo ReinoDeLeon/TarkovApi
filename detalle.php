709e9a8e6cb86a4172ddc544fa129f3431f3280b
Iván <ivanantonsagastizabal@gmail.com>
2022-01-20 11:00:18 +0100

    Changed css deleted js

--- detalle.php
+++ detalle.php
@@ -5,8 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php echo $_GET['name'] ?></title>
-    <link rel="stylesheet" href="css/style.css">
-    <script src="js/motor.js"></script>
+    <link rel="stylesheet" href="css/phpstyle.css">
 </head>
 <body id="detalle1">
 <header>
